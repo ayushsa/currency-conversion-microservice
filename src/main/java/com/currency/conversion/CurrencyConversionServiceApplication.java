7a18d7d723c35955fb53b9d68c25e593742d94a0
ayush099 <saxena.ayush09@gmail.com>
2020-03-26 14:02:35 +0530

    Change in Main class

--- src/main/java/com/currency/conversion/CurrencyConversionServiceApplication.java
+++ src/main/java/com/currency/conversion/CurrencyConversionServiceApplication.java
@@ -2,6 +2,8 @@ package com.currency.conversion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -11,15 +13,23 @@ import brave.sampler.Sampler;
 @SpringBootApplication
 @EnableFeignClients("com.currency.conversion")
 @EnableDiscoveryClient
-public class CurrencyConversionServiceApplication {
+public class CurrencyConversionServiceApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
 	}
+	
+	
 
 	@Bean
 	public Sampler defaultSampler() {
 		return Sampler.ALWAYS_SAMPLE;
 	}
+	
+	
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	        return builder.sources(CurrencyConversionServiceApplication.class);
+	    }
 
 }
